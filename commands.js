eaf985322b2ab1d8cd201f65af30eef99e74a744
Sergey <kort.dll@gmail.com>
2023-10-11 17:31:09 +0300

    fix windows comand

--- commands.js
+++ commands.js
@@ -1,7 +1,7 @@
 module.exports = {
   DISK_INFO: {
     WINDOWS:
-    "wmic logicaldisk get Caption,FreeSpace,Size,VolumeSerialNumber,Description  /format:list",
+    "wmic logicaldisk get Caption,FreeSpace,Size,Description  /format:list",
     UNIX: "df -P | awk 'NR > 1'",
   }
 };
